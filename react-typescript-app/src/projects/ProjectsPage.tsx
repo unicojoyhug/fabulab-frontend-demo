bda15e33f304ff7cca5164e434c6a426c6a3627f
Juyoung Choi <juyoung.choi@lego.com>
2023-03-31 14:37:15 +0200

    add assets and show the mocked data

--- react-typescript-app/src/projects/ProjectsPage.tsx
+++ react-typescript-app/src/projects/ProjectsPage.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { MOCK_PROJECTS } from './MockProjects';
 
 function ProjectsPage(){
-    return <h1>Projects</h1>;
+
+    let textFromJson = JSON.stringify(MOCK_PROJECTS);
+
+    return (
+        <>
+        <h1>Projects</h1>
+        <pre>{textFromJson}</pre>
+        </>
+    );
 }
 
 export default ProjectsPage;
