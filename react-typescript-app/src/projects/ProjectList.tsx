cdc5c76e27f5b407089e2de79f93043d9af8ba95
Juyoung Choi <juyoung.choi@lego.com>
2023-03-31 15:20:24 +0200

    show list as card

--- react-typescript-app/src/projects/ProjectList.tsx
+++ react-typescript-app/src/projects/ProjectList.tsx
@@ -6,13 +6,24 @@ interface ProjectListProps {
 }
 
 function ProjectList({ projects }: ProjectListProps) {
-    return (
-        <ul className='row'>
-            {projects.map( (project) => (
-                <li key={project.id}>{project.name}</li>
-            ))}
-        </ul>
-    );
+      return (
+            <div className="row">
+             {projects.map((project) => (
+               <div key={project.id} className="cols-sm">
+                  <div className="card">
+                    <img src={project.imageUrl} alt={project.name} />
+                    <section className="section dark">
+                      <h5 className="strong">
+                        <strong>{project.name}</strong>
+                      </h5>
+                      <p>{project.description}</p>
+                      <p>Budget : {project.budget.toLocaleString()}</p>
+                    </section>
+                  </div>
+                </div>
+              ))}
+            </div>
+          );
 }
 
 export default ProjectList;
